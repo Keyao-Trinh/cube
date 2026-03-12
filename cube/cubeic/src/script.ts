dea84b077d5ec883d20995af289554c001c128aa
342886108 <isabella.trinh@student.tdsb.on.ca>
2026-03-12 09:41:10 -0400

    commited

--- cube/cubeic/src/script.ts
+++ cube/cubeic/src/script.ts
@@ -53,14 +53,14 @@ form?.addEventListener("submit", (event) => {
         }
         // let x = (Math.cbrt(((-1 * q) / 2) + (Math.sqrt(((q / 2) * (q / 2)) + ((p / 3) * (p / 3) * (p / 3)))))) + (Math.cbrt(((-1 * q) / 2) - (Math.sqrt(((q / 2) * (q / 2)) + ((p / 3) * (p / 3) * (p / 3)))))) - (b / 3 * a);
     } else {
-        let theta = ((1 / 3) * (Math.acos((-q) / (2 *(Math.sqrt(-1*(p / 3) * (p/ 3) * (p / 3)))))));
-       
-        // rootone = (Math.cbrt(((-1 * q) / 2) + (Math.sqrt(((q / 2) * (q / 2)) + ((p / 3) * (p / 3) * (p / 3)))))) + (Math.cbrt(((-1 * q) / 2) - (Math.sqrt(((q / 2) * (q / 2)) + ((p / 3) * (p / 3) * (p / 3)))))) - (b / 3 * a);
-        roottwo = (((2 * (Math.sqrt(-p / 3))) * Math.cos(theta))-(b/(3*a)));
-        rootthree = (((2 * (Math.sqrt(-p / 3))) * Math.cos(theta+(Math.PI*2)/3))-(b/(3*a)));
-        rootone = (((2 * (Math.sqrt(-p / 3))) * Math.cos(theta+(Math.PI*4)/3))-(b/(3*a)));
+        let theta = ((1 / 3) * (Math.acos((-q) / (2 * (Math.sqrt(-1 * (p / 3) * (p / 3) * (p / 3)))))));
 
-        
+        // rootone = (Math.cbrt(((-1 * q) / 2) + (Math.sqrt(((q / 2) * (q / 2)) + ((p / 3) * (p / 3) * (p / 3)))))) + (Math.cbrt(((-1 * q) / 2) - (Math.sqrt(((q / 2) * (q / 2)) + ((p / 3) * (p / 3) * (p / 3)))))) - (b / 3 * a);
+        roottwo = (((2 * (Math.sqrt(-p / 3))) * Math.cos(theta)) - (b / (3 * a)));
+        rootthree = (((2 * (Math.sqrt(-p / 3))) * Math.cos(theta + (Math.PI * 2) / 3)) - (b / (3 * a)));
+        rootone = (((2 * (Math.sqrt(-p / 3))) * Math.cos(theta + (Math.PI * 4) / 3)) - (b / (3 * a)));
+
+
         // let rootthr = (2*(Math.sqrt((-1*p)/3)))*Math.cos(theta+((Math.PI*4)/3));
         //cube root unity
 
@@ -88,10 +88,10 @@ form?.addEventListener("submit", (event) => {
     ctx.moveTo(0, 0);
     let i = -200;
     let y = 0;
-    while (i<200){
-    y = ((a*i*i*i)+(b*i*i)+(c*i)+d);
-    ctx.lineTo(((i*0.1)+200),((y*0.1)+200));
-    i=(i+0.01);
+    while (i < 200) {
+        y = ((a * i * i * i) + (b * i * i) + (c * i) + d);
+        ctx.lineTo(((i * 0.1) + 200), ((y * 0.1) + 200));
+        i = (i + 0.01);
     }
     ctx.stroke();
 
